d9c17c7d613adc94e6fe6cfad200f6b65b501c86
Adolfo Marinucci <adospace@msn.com>
2023-01-21 16:24:34 +0100

    Initial commit

---
+++ src/ConfettiBuildPackage.cs
@@ -0,0 +1,100 @@
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
+using LottieSharp;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Media;
+using System.Windows;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ConfettiBuild
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.ConfettiBuildString)]
+    [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "ConfettiBuild", "General", 0, 0, true)]
+    [ProvideProfile(typeof(OptionsProvider.GeneralOptions), "ConfettiBuild", "General", 0, 0, true)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class ConfettiBuildPackage : ToolkitPackage
+    {
+        EnvDTE.DTE _dte;
+
+        Window _animationWindow;
+        LottieAnimationView _animationView;
+        DateTime _buildStart;
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            if (!(await GetServiceAsync(typeof(EnvDTE.DTE)) is EnvDTE.DTE dte))
+                return;
+
+            _dte = dte;
+
+            _dte.Events.BuildEvents.OnBuildBegin += BuildEvents_OnBuildBegin;
+            _dte.Events.BuildEvents.OnBuildDone += BuildEvents_OnBuildDone;
+
+            PrepareAnimationWindow();
+        }
+
+        private void BuildEvents_OnBuildBegin(EnvDTE.vsBuildScope Scope, EnvDTE.vsBuildAction Action)
+        {
+            _buildStart = DateTime.Now;
+            System.Diagnostics.Debug.WriteLine($"OnBuildBegin({_buildStart})");
+        }
+
+        private void BuildEvents_OnBuildDone(EnvDTE.vsBuildScope Scope, EnvDTE.vsBuildAction Action)
+        {
+            var elapsed = (DateTime.Now - _buildStart).TotalSeconds;
+            System.Diagnostics.Debug.WriteLine($"OnBuildDone({elapsed} seconds) MinBuildDuration:{General.Instance.MinBuildDuration} secs");
+
+            if (elapsed >= General.Instance.MinBuildDuration)
+            {
+                if (Action == EnvDTE.vsBuildAction.vsBuildActionBuild || Action == EnvDTE.vsBuildAction.vsBuildActionRebuildAll)
+                {
+                    ShowAnimation();
+                }
+            }
+        }
+
+        private void PrepareAnimationWindow()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            _animationView = new LottieAnimationView
+            {
+                AutoPlay = false,
+                FileName = $@".\Animations\{General.Instance.Animation.ToString().ToLowerInvariant()}.json",
+                Background = Brushes.Transparent,
+                RepeatCount = 0,
+            };
+
+            _animationWindow = new Window
+            {
+                Title = "Confetti",
+                VerticalContentAlignment = VerticalAlignment.Stretch,
+                HorizontalContentAlignment = HorizontalAlignment.Stretch,
+                AllowsTransparency = true,
+                WindowStyle = WindowStyle.None,
+                WindowState = WindowState.Maximized,
+                Background = Brushes.Transparent,
+                Content = _animationView,
+                Topmost = true,
+            };
+
+            _animationWindow.Show();
+        }
+
+        private void ShowAnimation()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            System.Diagnostics.Debug.WriteLine("Showing animation...");
+            _animationView.PlayAnimation();
+        }
+    }
+}
---
+++ src/Options/General.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace ConfettiBuild
+{
+    internal partial class OptionsProvider
+    {
+        // Register the options with this attribute on your package class:
+        // [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "ConfettiBuild", "General", 0, 0, true, SupportsProfiles = true)]
+        [ComVisible(true)]
+        public class GeneralOptions : BaseOptionPage<General> { }
+    }
+
+    public class General : BaseOptionModel<General>
+    {
+        private int _minBuildDuration = 1;
+
+        [Category("General")]
+        [DisplayName("Minimum build duration (sec)")]
+        [Description("Show animation only if build duration is more than the specified value in second.")]
+        [DefaultValue(1)]
+        public int MinBuildDuration
+        {
+            get => _minBuildDuration;
+            set
+            {
+                _minBuildDuration = Math.Max(0, value);
+            }
+        }
+
+        [Category("General")]
+        [DisplayName("Animation")]
+        [Description("Select the animation to play (restart required).")]
+        [DefaultValue(Animation.Confetti1)]
+        public Animation Animation { get; set; }
+    }
+
+    public enum Animation
+    {
+        Confetti1,
+
+        Confetti2,
+
+        Confetti3
+    }
+}
